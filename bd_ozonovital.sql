44bd07c29191aee1328d12ecc5b8faaef3f9e761
danilux <colmenarezdaniel177@gmail.com>
2026-05-09 19:02:34 -0400

    Base de datos Script Nuevo

---
+++ bd_ozonovital.sql
@@ -0,0 +1,373 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2026 a las 00:59:17
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_ozonovital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `antecedentes_medicos`
+--
+
+CREATE TABLE `antecedentes_medicos` (
+  `id` int(11) NOT NULL,
+  `paciente_id` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha` datetime NOT NULL,
+  `tipo_antecedente` enum('Familiar','Personal Patológico','Personal No Patológico','Quirúrgico','Alérgico') DEFAULT NULL,
+  `notas_adicionales` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cita`
+--
+
+CREATE TABLE `cita` (
+  `id` int(11) NOT NULL,
+  `paciente_id` int(11) NOT NULL,
+  `especialista_id` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `status` enum('Programada','Confirmada','En Espera','En Consulta','Completada','Cancelada','No Asistió','Reprogramada') DEFAULT NULL,
+  `nota` varchar(200) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cita`
+--
+
+INSERT INTO `cita` (`id`, `paciente_id`, `especialista_id`, `fecha`, `status`, `nota`) VALUES
+(9, 22, 12, '2026-05-24 00:00:00', 'Programada', 'p;io;yto8;');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialidad`
+--
+
+CREATE TABLE `especialidad` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(75) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especialidad`
+--
+
+INSERT INTO `especialidad` (`id`, `nombre`) VALUES
+(1, 'Ginecologia'),
+(2, 'Cardiologia'),
+(5, 'Pediatria');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialista`
+--
+
+CREATE TABLE `especialista` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `especialidad_id` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especialista`
+--
+
+INSERT INTO `especialista` (`id`, `nombre`, `especialidad_id`) VALUES
+(12, 'Pediatria', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estatus_cita`
+--
+
+CREATE TABLE `estatus_cita` (
+  `Id_Estatus` int(11) NOT NULL,
+  `Descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historia_clinica`
+--
+
+CREATE TABLE `historia_clinica` (
+  `id` int(11) NOT NULL,
+  `cita_id` int(11) NOT NULL,
+  `motivo_consulta` text NOT NULL,
+  `tratamiento` text NOT NULL,
+  `observaciones` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `cedula` int(11) NOT NULL,
+  `fecha_nacimiento` datetime NOT NULL,
+  `usuario_id` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `paciente`
+--
+
+INSERT INTO `paciente` (`id`, `nombre`, `cedula`, `fecha_nacimiento`, `usuario_id`) VALUES
+(22, 'Pediatria', 14093340, '2026-03-01 00:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id`, `nombre`) VALUES
+(1, 'Administrador'),
+(2, 'Invitado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_antecedente`
+--
+
+CREATE TABLE `tipo_antecedente` (
+  `Id_Antecedente` int(11) NOT NULL,
+  `Descripcion` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `login` text NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  `status` int(11) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `login`, `password_hash`, `rol_id`, `status`) VALUES
+(1, 'admin@local.com', '$2y$10$Ytj7ygxUSpDuaqVV27pUtOkvoWndrXxP6LztwcLO5Wyu6tKYcTUGO', 1, 0),
+(5, 'Invitado@local.com', '$2y$10$SRG9sokcf6oYNCYDX4y6SO674hdARi3MUz/1fqp1kbBAfdVQmdT8K', 2, 1),
+(6, 'lol', '$2y$10$FzCzGSmfm74qQ.sxWU6n1Or9Z3hQjzGWwHEbRwDU.YpSG.stkBDiC', 2, 1),
+(7, 'daniel', '$2y$10$.tvPc/HM4/2l0asmcaQPuub15JJfN2uUGWP9KSCPQZhyt9yYY57dS', 1, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `antecedentes_medicos`
+--
+ALTER TABLE `antecedentes_medicos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_antecedentes_medicos_paciente` (`paciente_id`);
+
+--
+-- Indices de la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `FK_Paciente` (`paciente_id`) USING BTREE,
+  ADD KEY `FK_Especialista` (`especialista_id`) USING BTREE;
+
+--
+-- Indices de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `especialista`
+--
+ALTER TABLE `especialista`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `FK_especialista_especialidad` (`especialidad_id`);
+
+--
+-- Indices de la tabla `estatus_cita`
+--
+ALTER TABLE `estatus_cita`
+  ADD PRIMARY KEY (`Id_Estatus`);
+
+--
+-- Indices de la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_historia_clinica_cita` (`cita_id`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `FK_paciente_usuarios` (`usuario_id`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indices de la tabla `tipo_antecedente`
+--
+ALTER TABLE `tipo_antecedente`
+  ADD PRIMARY KEY (`Id_Antecedente`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `FK_ROL` (`rol_id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cita`
+--
+ALTER TABLE `cita`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `especialista`
+--
+ALTER TABLE `especialista`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `estatus_cita`
+--
+ALTER TABLE `estatus_cita`
+  MODIFY `Id_Estatus` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_antecedente`
+--
+ALTER TABLE `tipo_antecedente`
+  MODIFY `Id_Antecedente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `antecedentes_medicos`
+--
+ALTER TABLE `antecedentes_medicos`
+  ADD CONSTRAINT `FK_antecedentes_medicos_paciente` FOREIGN KEY (`paciente_id`) REFERENCES `paciente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD CONSTRAINT `FK_Especialista` FOREIGN KEY (`especialista_id`) REFERENCES `especialista` (`id`),
+  ADD CONSTRAINT `FK_Paciente` FOREIGN KEY (`paciente_id`) REFERENCES `paciente` (`id`);
+
+--
+-- Filtros para la tabla `especialista`
+--
+ALTER TABLE `especialista`
+  ADD CONSTRAINT `FK_especialista_especialidad` FOREIGN KEY (`especialidad_id`) REFERENCES `especialidad` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  ADD CONSTRAINT `FK_historia_clinica_cita` FOREIGN KEY (`cita_id`) REFERENCES `cita` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD CONSTRAINT `FK_paciente_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `FK_rol` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
